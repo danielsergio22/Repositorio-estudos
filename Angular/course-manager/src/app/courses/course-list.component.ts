5a5691f0e20c9d6b75e280644be72c9a3a6a9f2b
danielsergio22 <ds.daniel.sergio@gmail.com>
2022-05-11 11:49:06 -0300

    Primeiros passos com Angular

---
+++ Angular/course-manager/src/app/courses/course-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-Course-list',
+    template: './course-list.component.html'
+})
+
+export class CourseListComponent{
+
+    
+}
